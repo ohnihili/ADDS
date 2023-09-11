847132a4ae5b289055a7dfe3c64bf71ad051a31e
ohnihili <bpfsignorelli@gmail.com>
2023-09-11 18:56:16 +0930

    bignum done

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -37,10 +37,50 @@ std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2)
 
 std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2)
 {
-    return num1;
+    std::list<int> result;
+    int borrow = 0;
+
+    while (!num1.empty() || !num2.empty()) {
+        int x = borrow;
+        if (!num1.empty()) {
+            x += num1.back();
+            num1.pop_back();
+        }
+        if (!num2.empty()) {
+            x -= num2.back();
+            num2.pop_back();
+        }
+        if (x < 0) {
+            x += 10;
+            borrow = -1;
+        } else {
+            borrow = 0;
+        }
+        result.push_front(x);
+    }
+
+    while (!result.empty() && result.front() == 0) {
+        result.pop_front(); // Remove leading zeros
+    }
+
+    return result;
 }
 
 std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2)
 {
-    return num1;
+    std::list<int> result;
+    int digit = num2.front();
+    int carry = 0;
+
+    for (int x : num1) {
+        int product = x * digit + carry;
+        carry = product / 10;
+        result.push_back(product % 10);
+    }
+
+    if (carry > 0) {
+        result.push_back(carry);
+    }
+
+    return result;
 }
